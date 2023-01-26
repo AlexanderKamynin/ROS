42bf7bbccc892021d2a49df413b13e57a9e3ac6c
AlexanderKamynin <kamyninsasa@gmail.com>
2023-01-26 22:18:59 +0300

    My turtle is run!

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/alexander/workspace/src
+# Build directory: /home/alexander/workspace/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
+subdirs("talkers")
---
+++ build/talkers/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/alexander/workspace/src/talkers
+# Build directory: /home/alexander/workspace/build/talkers
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
